b7e22c06febb61ddeb1ebd028dfb9f96ac256504
Yuki Sakuma <kumasan.04.24.14@gmail.com>
2021-03-28 19:08:57 +0900

    [Improve] still segv in checker.c

--- ch_srcs/checker.c
+++ ch_srcs/checker.c
@@ -1,16 +1,33 @@
 #include "../includes/checker.h"
 
+t_stack
+	*lstnew(int num)
+{
+	t_stack *ret;
+
+	ret = NULL;
+	ret->num = num;
+	ret->prev = ret;
+	ret->next = ret;
+	return (ret);
+}
+
 void
-	pack_stack(t_checker *checker, int ac, char **av)
+	pack_stack(t_checker *checker, char **av)
 {
 	//TODO: pack av to stack
+	int	first_num;
+
+	first_num = ft_atoi(av[1]);
+	checker->top = lstnew(first_num);
 }
 
 int
 	main(int ac, char **av)
 {
-	t_checker	*checker;
+	t_checker	checker;
 
-	pack_stack(checker, ac, av);
+	pack_stack(&checker, av);
+	printf("ac:%d,num:%d\n", ac, checker.top->num);
 	return (0);
 }
--- includes/checker.h
+++ includes/checker.h
@@ -6,7 +6,7 @@
 /*   By: ysakuma <ysakuma@student.42tokyo.j>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/19 12:19:38 by ysakuma           #+#    #+#             */
-/*   Updated: 2021/03/28 16:41:16 by ysakuma          ###   ########.fr       */
+/*   Updated: 2021/03/28 19:06:29 by ysakuma          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,18 +14,21 @@
 # define _CHECKER_H
 # include <unistd.h>
 # include <stdlib.h>
+# include <stdio.h>
 # include "../get_next_line/get_next_line.h"
 # include "../libft/libft.h"
 
 typedef struct	s_stack
 {
 	int	num;
-	s_stack	*prev;
-	s_stack	*next;
+	struct s_stack	*prev;
+	struct s_stack	*next;
 }				t_stack;
 
 typedef struct	s_checker
 {
+	t_stack	*top;
+	t_stack	*end;
 	t_stack	*head;
 	t_stack	*tail;
 	int		len;
