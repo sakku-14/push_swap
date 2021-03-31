b4ba565c722a0466db16907ed5d327c7b0bf4d34
Yuki Sakuma <kumasan.04.24.14@gmail.com>
2021-04-01 08:31:30 +0900

    [Improve] add head tail arrow in display_stack

--- ch_srcs/checker.c
+++ ch_srcs/checker.c
@@ -42,6 +42,7 @@ int
 			return (FALSE);
 		i++;
 	}
+	st->b.tail = st->b.head;
 	return (TRUE);
 }
 
@@ -96,11 +97,23 @@ void
 	printf("\n*****************\n>>>>>Stack_A<<<<<\n*****************\n");
 	i = 1;
 	lst = st->a.head;
+	if (lst == st->a.head && lst == st->a.tail)
+		printf("h&t--->");
+	else if (lst == st->a.head)
+		printf("head-->");
+	else
+		printf("       ");
 	printf("%3d->%3d | addr->%p | prev->%p | next->%p\n", i, lst->num, lst, lst->prev, lst->next);
 	i++;
 	lst = lst->next;
 	while (lst != st->a.head)
 	{
+		if (lst == st->a.head && lst == st->a.tail)
+			printf("h&t--->");
+		else if (lst == st->a.tail)
+			printf("tail-->");
+		else
+			printf("       ");
 		printf("%3d->%3d | addr->%p | prev->%p | next->%p\n", i, lst->num, lst, lst->prev, lst->next);
 		i++;
 		lst = lst->next;
@@ -109,16 +122,27 @@ void
 	printf("\n*****************\n>>>>>Stack_B<<<<<\n*****************\n");
 	i = 1;
 	lst = st->b.head;
+	if (lst == st->b.head && lst == st->b.tail)
+		printf("h&t--->");
+	else if (lst == st->b.head)
+		printf("head-->");
+	else
+		printf("       ");
 	printf("%3d->%3d | addr->%p | prev->%p | next->%p\n", i, lst->num, lst, lst->prev, lst->next);
 	i++;
 	lst = lst->next;
 	while (lst != st->b.head)
 	{
+		if (lst == st->b.head && lst == st->b.tail)
+			printf("h&t--->");
+		else if (lst == st->b.tail)
+			printf("tail-->");
+		else
+			printf("       ");
 		printf("%3d->%3d | addr->%p | prev->%p | next->%p\n", i, lst->num, lst, lst->prev, lst->next);
 		i++;
 		lst = lst->next;
 	}
-
 }
 
 int
