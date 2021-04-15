45f835f1f397639824183c957631f8ca7ca21eed
Yuki Sakuma <kumasan.04.24.14@gmail.com>
2021-04-15 12:48:00 +0900

    [Improve] pattern for counter=1

--- ps_srcs/push_swap.c
+++ ps_srcs/push_swap.c
@@ -1035,11 +1035,90 @@ void
 }
 
 void
+	check_f_len(t_stacks *st, int *f_len, t_dlist *ptr, int num)
+{
+	while (st->nums[num] != ptr->num)
+	{
+		(*f_len)++;
+		ptr = ptr->next;
+	}
+}
+
+void
+	check_s_len(t_stacks *st, int *s_len, t_dlist *ptr, int num)
+{
+	while (st->nums[num] != ptr->num)
+	{
+		(*s_len)++;
+		ptr = ptr->prev;
+	}
+}
+
+void
+	push_fs_act(t_stacks *st, int *f_len, int *s_len, int num)
+{
+	if (f_len <= s_len)
+	{
+		while (st->a.head->num != st->nums[num])
+		{
+			rotate(&st->a);
+			write(1, "ra\n", 3);
+		}
+		push(st, 'b');
+		write(1, "pb\n", 3);
+	}
+	else
+	{
+		while (st->a.head->num != st->nums[num])
+		{
+			rev_rotate(&st->a);
+			write(1, "rra\n", 4);
+		}
+		push(st, 'b');
+		write(1, "pb\n", 3);
+	}
+}
+
+void
+	push_first_second(t_stacks *st, int *f_len, int *s_len, int num)
+{
+	t_dlist	*ptr;
+
+	*f_len = 0;
+	*s_len = 0;
+	ptr = st->a.head;
+	check_f_len(st, f_len, ptr, num);
+	ptr = st->a.tail;
+	check_f_len(st, s_len, ptr, num);
+	push_fs_act(st, f_len, s_len, num);
+}
+
+void
+	push_min_to_a(t_stacks *st)
+{
+	int	f_len;
+	int	s_len;
+
+	push_first_second(st, &f_len, &s_len, 0);
+	push_first_second(st, &f_len, &s_len, 1);
+}
+
+void
+	set_five(t_stacks *st)
+{
+	push_min_to_a(st);
+	set_three_asc(st);
+	w_push(st, 'a');
+}
+
+void
 	sort_five(t_stacks *st)
 {
 	int	counter;
 
 	counter = asc_count(st);
+	if (counter == 1)
+		set_five(st);
 	if (counter == 2)
 		set_three(st);
 	else if (counter == 3)
