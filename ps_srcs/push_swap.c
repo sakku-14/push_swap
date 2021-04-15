93c9e9853648a42556bfc839f4d1ad3dd9cf879a
Yuki Sakuma <kumasan.04.24.14@gmail.com>
2021-04-15 12:01:14 +0900

    [Improve] check_a_asc

--- ps_srcs/push_swap.c
+++ ps_srcs/push_swap.c
@@ -851,6 +851,15 @@ void
 		write(1, "pa\npa\n", 6);
 }
 
+void
+	inc_i_for_5(int *i)
+{
+	if (*i != 4)
+		(*i)++;
+	else
+		*i = 0;
+}
+
 int
 	check_a_asc(t_stacks *st)
 {
@@ -865,17 +874,19 @@ int
 		i++;
 	while (counter != 3)
 	{
-		if (st->nums[i] != ptr->num)
-			return (FALSE);
-		counter++;
-		if (i < 5)
-			i++;
+		if (st->nums[i] == st->b.head->num || st->nums[i] == st->b.tail->num)
+			inc_i_for_5(&i);
 		else
-			i = 0;
-		if (ptr == st->a.tail)
-			ptr = st->a.head;
-		else
-			ptr = ptr->next;
+		{
+			if (st->nums[i] != ptr->num)
+				return (FALSE);
+			counter++;
+			inc_i_for_5(&i);
+			if (ptr == st->a.tail)
+				ptr = st->a.head;
+			else
+				ptr = ptr->next;
+		}
 	}
 	return (TRUE);
 }
