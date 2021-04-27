12f8ddef7cfc69ca9f68dcff7aeea0eb0acefa8d
Yuki Sakuma <kumasan.04.24.14@gmail.com>
2021-04-27 14:24:38 +0900

    [Improve] makefile

---
+++ Makefile
@@ -0,0 +1,26 @@
+PUSH_SWAP		= push_swap
+CHECKER			= checker
+PUSH_SWAP_DIR	= ./ps_srcs
+CHECKER_DIR		= ./ch_srcs
+
+all:
+	make -C $(PUSH_SWAP_DIR)
+	cp $(PUSH_SWAP_DIR)/$(PUSH_SWAP) ./
+	make -C $(CHECKER_DIR)
+	cp $(CHECKER_DIR)/$(CHECKER) ./
+
+clean:
+	make -C $(CHECKER_DIR) clean;
+	make -C $(PUSH_SWAP_DIR) clean;
+
+fclean:
+	make -C $(CHECKER_DIR) fclean;
+	make -C $(PUSH_SWAP_DIR) fclean;
+	rm -f $(PUSH_SWAP)
+	rm -f $(CHECKER)
+
+re: fclean all
+
+bonus : all
+
+.PHONY: all clean fclean re bonus
--- includes/checker.h
+++ includes/checker.h
@@ -44,4 +44,73 @@ typedef struct	s_stacks
 	int		len;
 }				t_stacks;
 
+int
+	check_av(int ac, char **av);
+int
+	check_stack_a(t_stacks *st);
+int
+	check_stack(t_stacks *st);
+int
+	check_duplicate(t_stacks *st);
+void
+	display_array(int *x, int len);//debug
+void
+	display_stack(t_stacks *st, char *str);//debug
+int
+	ft_dlstadd_back(t_stack *stack, int num, int exist);
+void
+	ft_dlstnew(t_stack *stack, int num, int exist);
+int
+	print_error(void);
+int
+	exit_error(void);
+int
+	check_order(char *buf);
+void
+	do_order(t_stacks *st, char *buf);
+int
+	get_do_ord(t_stacks *st);
+void
+	pack_array(t_stacks *st);
+int
+	pack_sort_array(t_stacks *st);
+int
+	build_stack_b(t_stacks *st);
+int
+	check_over_int_range(char *str);
+int
+	pack_stack(t_stacks *st, char **av);
+int
+	push_available(t_stacks *st, char to);
+void
+	switch_exist(t_dlist *lst);
+void
+	push_lst(t_stack *from, t_stack *to);
+void
+	after_push(t_stack *stack);
+void
+	push(t_stacks *st, char to);
+void
+	rotate(t_stack *stack);
+void
+	w_rotate(t_stacks *st);
+void
+	rev_rotate(t_stack *stack);
+void
+	w_rev_rotate(t_stacks *st);
+void
+	swap_num(int *nums, int l, int r);
+int
+	swap_array(int *nums, int pivot, int *l, int *r);
+void
+	q_sort_array(int *nums, int left, int right);
+void
+	sort_array(int *nums, int len);
+int
+	check_swap_available(t_stack *stack);
+void
+	swap(t_stack *stack);
+void
+	w_swap(t_stacks *st);
+
 #endif
