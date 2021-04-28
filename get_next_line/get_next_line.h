cf98f35572081e666319a264982433d1ffaeaa2c
Yuki Sakuma <kumasan.04.24.14@gmail.com>
2021-04-28 15:12:47 +0900

    [Improve] get_next_line for norm

--- get_next_line/get_next_line.h
+++ get_next_line/get_next_line.h
@@ -6,12 +6,12 @@
 /*   By: ysakuma <ysakuma@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/22 13:43:50 by ysakuma           #+#    #+#             */
-/*   Updated: 2021/04/28 15:07:29 by ysakuma          ###   ########.fr       */
+/*   Updated: 2021/04/28 15:12:33 by ysakuma          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef _GET_NEXT_LINE_H
-# define _GET_NEXT_LINE_H
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
 # include <unistd.h>
 # include <stdlib.h>
 
@@ -26,7 +26,7 @@ size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
 size_t	ft_strlen(const char *s);
 void	*ft_memchr(const void *s, int c, size_t n);
 size_t	ft_strlcat(char *dst, const char *src, size_t dstsize);
-int	first_set_save(char **save, int fd);
-int	read_buf(ssize_t *len, int fd, char *buf, int buf_size);
+int		first_set_save(char **save, int fd);
+int		read_buf(ssize_t *len, int fd, char *buf, int buf_size);
 
 #endif
