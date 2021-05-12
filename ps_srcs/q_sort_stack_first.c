fd4178adc98b733daa031c9fecdb8277cabf5ded
Yuki Sakuma <kumasan.04.24.14@gmail.com>
2021-05-12 18:15:59 +0900

    [Imporove] add make first teq but no work some

--- ps_srcs/q_sort_stack_first.c
+++ ps_srcs/q_sort_stack_first.c
@@ -16,6 +16,14 @@ void
 			//write(1, "pb\n", 3);
 			if (add_oper(st, 5) == FALSE)
 				exit_error();
+			//TODO: check below
+			if (st->b.head->num <= st->nums[3] && st->b.head != st->b.tail)
+			{
+				rotate(&st->b);
+				if (add_oper(st, 7) == FALSE)
+					exit_error();
+				counter++;
+			}
 		}
 		else
 		{
